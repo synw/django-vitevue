95ef79b1cfced16f1b2ba9158b509773e4210f1b
Win <synwe@yahoo.com>
2017-03-14 16:08:33 +0000

    Added the preserve parameter to the flush function

--- vv/templates/vv/app.js
+++ vv/templates/vv/app.js
@@ -16,25 +16,35 @@ const app = new Vue({
 		{% for appname, parts in apps.items %}
 			{% include parts.methods %}
 		{% endfor %}
-		flush: function() {
+		flush: function(preserve) {
 			//console.log("FLUSH");
 			for (i=0;i<this.active.length;i++) {
 				v = app[this.active[i]];
-				//console.log("flushing "+this.active[i])
-				var t = typeOf(v);
-				if (t === "string") {
-					app[this.active[i]] = ""
-				} else if (t === "array") {
-					app[this.active[i]] = [];
-				} else if (t === "object") {
-					app[this.active[i]] = {}
+				console.log("Preserve: "+this.active[i]+" / "+preserve);
+				if (this.active[i] != preserve) {
+					console.log("flushing "+this.active[i])
+					var t = typeOf(v);
+					if (t === "string") {
+						console.log(this.active[i]+ " -> Flushing string");
+						app[this.active[i]] = ""
+					} else if (t === "array") {
+						console.log(this.active[i]+ " -> Flushing array");
+						app[this.active[i]] = [];
+					} else if (t === "object") {
+						console.log(this.active[i]+ " -> Flushing object");
+						app[this.active[i]] = {}
+					}
+					delete(this.active[i]);
+				} else {
+					console.log("Preserving "+this.active[i])
 				}
-				delete(this.active[i]);
 			}
+			console.log("After flush active: "+this.active);
 		},
 		activate: function(args) {
-			//console.log("ACTIVATE "+args);
+			console.log("ACTIVATE "+args);
 			this.active = args;
+			console.log("After activate active: "+this.active);
 		},
 		loadData: function(resturl, action) {
 			promise.get(resturl).then(function(error, data, xhr) {
