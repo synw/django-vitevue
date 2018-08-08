8ef0dcb43b190bfc79b6657a385cb290553bbd64
Win <synwe@yahoo.com>
2018-08-08 08:55:50 +0000

    Remove old code in store

--- vv/static/vv/vvstore.js
+++ vv/static/vv/vvstore.js
@@ -1,7 +1,6 @@
 const store = new Vuex.Store({
 	state: {
 		active: [],
-    	pageTitle: "",
 	},
 	mutations: {
 		activate(state, elems) {
@@ -17,12 +16,6 @@ const store = new Vuex.Store({
 				}
 			}
 		},
-		setPageTitle: function(state, title) {
-			state.pageTitle = title;
-		},
-		fillSidebar: function(state, content) {
-			state.sidebar = content;
-		},
 	},
 	actions: {
 		activate: function(context, elems) {
@@ -31,16 +24,8 @@ const store = new Vuex.Store({
 		deactivate: function(context, elems) {
 			context.commit("deactivate", elems);
 		},
-		setPageTitle: function(context, title) {
-			context.commit("setPageTitle", title);
-		},
-		fillSidebar: function(context, content) {
-			context.commit("fillSidebar", content);
-		},
 	},
 	getters: {
 		active: function(state) { return state.active },
-    	pageTitle: function(state) { return state.pageTitle },
-    	sidebar: function(state) { return state.sidebar },
 	},
 });
