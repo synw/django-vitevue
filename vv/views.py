2373f639182cc390a0f860dd6395d13b2274b213
Win <synwe@yahoo.com>
2018-08-26 12:48:18 +0000

    Improve PostFormView

--- vv/views.py
+++ vv/views.py
@@ -2,7 +2,7 @@
 import json
 from goerr import Err
 from django.views.generic import TemplateView
-from django.views.generic.edit import CreateView
+from django.views.generic import FormView
 from django.http import JsonResponse
 from django.utils.html import escape
 from vv.utils import check_csrf
@@ -12,21 +12,21 @@ class IndexView(TemplateView):
     template_name = "vv/index.html"
 
 
-class PostFormView(CreateView, Err):
-    model = None
-    fields = []
+class PostFormView(FormView, Err):
 
     def action(self, request, clean_data):
         pass
 
     def post(self, request, *args, **kwargs):
+        super(FormView, self).post(request, *args, **kwargs)
         if check_csrf(request) == False:
             return JsonResponse({"error": 1})
         try:
             data = json.loads(self.request.body.decode('utf-8'))
             clean_data = {}
-            for field in self.fields:
-                clean_data[field] = escape(data[field])
+            for field in data:
+                if field != "csrfmiddlewaretoken":
+                    clean_data[field] = escape(data[field])
             data = self.action(self.request, clean_data)
             resp = {"error": 0}
             if data is not None:
