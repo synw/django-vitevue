95f02ccbb80cff0b1df738a868d9a5f216302b45
synw <synwx@protonmail.com>
2021-11-17 13:54:09 +0100

    Upgrade package setup

--- setup.py
+++ setup.py
@@ -1,35 +1,5 @@
-from os import path
-from setuptools import setup, find_packages
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup
 
-
-version = __import__("vv").__version__
-
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
-    long_description = f.read()
-
-setup(
-    name="django-vitevue",
-    packages=find_packages(),
-    include_package_data=True,
-    version=version,
-    description="Manage Vitejs frontends for Django",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    author="synw",
-    author_email="synwe@yahoo.com",
-    url="https://github.com/synw/django-vitevue",
-    download_url="https://github.com/synw/django-vitevue/releases/tag/" + version,
-    keywords=["django", "vitejs"],
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Framework :: Django :: 3.0",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.8",
-    ],
-    install_requires=[
-        "django-introspection",
-    ],
-    zip_safe=False,
-)
+setup()
